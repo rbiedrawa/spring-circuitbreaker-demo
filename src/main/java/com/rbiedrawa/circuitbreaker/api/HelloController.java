cf9aa4ce674356b2d5845f398684fcccd9d7746c
rbiedrawa <biedrawa.rafal@gmail.com>
2021-04-07 09:25:58 +0200

    Init commit

---
+++ src/main/java/com/rbiedrawa/circuitbreaker/api/HelloController.java
@@ -0,0 +1,17 @@
+package com.rbiedrawa.circuitbreaker.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+	@GetMapping Mono<String> hello() {
+		return Mono.just("Hello World!");
+	}
+
+}
